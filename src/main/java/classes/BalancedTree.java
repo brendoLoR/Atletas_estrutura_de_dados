f06b83abe24071f42980bbf320a50f82d3667d31
brendo <brendoja@gmail.com>
2021-06-15 21:17:28 -0300

    implementação do metodo de multipla inserção na arvore

--- src/main/java/classes/BalancedTree.java
+++ src/main/java/classes/BalancedTree.java
@@ -4,159 +4,169 @@
  * and open the template in the editor.
  */
 package classes;
+
 import java.util.*;
- /**
+
+/**
  *
  * @author brendoja
  */
 public class BalancedTree {
+
     private Node root;
-    public String[] last_node;
-    
-    private int getKey(String name){
+
+    private int getKey(String name) {
         return name.hashCode();
     }
-    
-    public void add(String[] key){
-        int key_value = this.getKey(key[0]);
-        this.last_node = key;
-        this.setRoot(add(this.getRoot(), key_value));
+
+    public void addMultAtletas(Atleta[] atletas) {
+        for (Atleta atleta : atletas) {
+            int key_value = atleta.getHash();
+            this.setRoot(add(this.getRoot(), key_value, atleta));
+        }
     }
-    
-    public void remove(int key){
+
+    public void add(String[] atleta_str) {
+        int key_value = this.getKey(atleta_str[0]);
+        this.setRoot(add(this.getRoot(), key_value, new Atleta(atleta_str)));
+    }
+
+    public void remove(int key) {
         this.setRoot(remove(this.getRoot(), key));
     }
-    
-    public Node search(int key){
+
+    public Node search(int key) {
         return this.search(this.getRoot(), key);
     }
-    
-    public Atleta[] inOrder(Node node, List<Atleta> aux){
-        if(node != null){
+
+    public Atleta[] inOrder(Node node, List<Atleta> aux) {
+        if (node != null) {
             this.inOrder(node.getLeft(), aux);
 //            aux.add(Arrays.toString(node.getAtleta().getArgs()));
             aux.add(node.getAtleta());
             this.inOrder(node.getRight(), aux);
         }
-        return (Atleta[])aux.toArray(new Atleta[aux.size()]);
+        return (Atleta[]) aux.toArray(new Atleta[aux.size()]);
     }
-    
-    public Integer[] beforeOrder(Node node, List<Integer> aux){
-        if(node != null){
-            aux.add(node.getValue());
+
+    public Atleta[] beforeOrder(Node node, List<Atleta> aux) {
+        if (node != null) {
+            aux.add(node.getAtleta());
             this.beforeOrder(node.getLeft(), aux);
             this.beforeOrder(node.getRight(), aux);
         }
-        return (Integer[])aux.toArray(new Integer[aux.size()]);
+        return (Atleta[]) aux.toArray(new Atleta[aux.size()]);
     }
-    
-    public Integer[] afterOrder(Node node, List<Integer> aux){
-        if(node != null){
+
+    public Atleta[] afterOrder(Node node, List<Atleta> aux) {
+        if (node != null) {
             this.afterOrder(node.getLeft(), aux);
             this.afterOrder(node.getRight(), aux);
-            aux.add(node.getValue());
+            aux.add(node.getAtleta());
         }
-        return (Integer[])aux.toArray(new Integer[aux.size()]);
+        return (Atleta[]) aux.toArray(new Atleta[aux.size()]);
     }
-        
-    private Node search(Node node, int key){
-        if(this.isEmpty(node) || node.getValue() == key){
+
+    private Node search(Node node, int key) {
+        if (this.isEmpty(node) || node.getHash_id() == key) {
             return node;
-        }else{
-            return node.getValue() < key ? this.search(node.getRight(), key) :
-                    this.search(node.getLeft(), key);
+        } else {
+            return node.getHash_id() < key ? this.search(node.getRight(), key)
+                    : this.search(node.getLeft(), key);
         }
-    } 
-    
-    private Node add(Node node, int key){
-        if (node == null){
-            return new Node(key, this.last_node);
-        }else if (node.getValue() > key){
-            node.setLeft(add(node.getLeft(), key));
-            
-        }else if (node.getValue() < key){
-            node.setRight(add(node.getRight(), key));
-        }else{
+    }
+
+    private Node add(Node node, int hash_id, Atleta atleta) {
+        if (node == null) {
+            return new Node(hash_id, atleta);
+        } else if (node.getHash_id() > hash_id) {
+            node.setLeft(add(node.getLeft(), hash_id, atleta));
+
+        } else if (node.getHash_id() < hash_id) {
+            node.setRight(add(node.getRight(), hash_id, atleta));
+        } else {
             throw new RuntimeException("Duplicate key value");
         }
         return this.rebalance(node);
     }
-  
-    private Node remove(Node node, int key){
+
+    private Node remove(Node node, int key) {
         Node aux;
         //search recursivaly for the right node.
-        
-        if(node.getValue() == key){
-            if(node.getLeft().getRight() == null){
+
+        if (node.getHash_id() == key) {
+            if (node.getLeft().getRight() == null) {
                 aux = node.getLeft();
                 node.setLeft(null);
-            }else {
+            } else {
                 aux = this.takeLastLeftChild(node.getLeft());
             }
-            node.setValue(aux.getValue());
-            if(aux.getRight() != null){
+            node.setHash_id(aux.getHash_id());
+            if (aux.getRight() != null) {
                 Node r = aux.getRight();
-                aux.setValue(r.getValue());
-                aux.setRight(null);                
+                aux.setHash_id(r.getHash_id());
+                aux.setRight(null);
             }
-        }else if(node.getValue() < key){
-            if(node.getRight().getValue() == key && node.getRight().getRight() == null
-                    && node.getRight().getLeft() == null ){
+        } else if (node.getHash_id() < key) {
+            if (node.getRight().getHash_id() == key && node.getRight().getRight() == null
+                    && node.getRight().getLeft() == null) {
                 node.setRight(null);
-            }else{
+            } else {
                 this.remove(node.getRight(), key);
             }
-            
-        }else if(node.getValue() > key){
-            if(node.getLeft().getValue() == key && node.getLeft().getRight() == null
-                    && node.getLeft().getLeft() == null){
+
+        } else if (node.getHash_id() > key) {
+            if (node.getLeft().getHash_id() == key && node.getLeft().getRight() == null
+                    && node.getLeft().getLeft() == null) {
                 node.setLeft(null);
-            }else{
+            } else {
                 this.remove(node.getLeft(), key);
             }
-        }else{
+        } else {
             throw new RuntimeException("key not found");
         }
-        
+
         return this.rebalance(node);
     }
-    
+
     private Node takeLastLeftChild(Node node) {
-        if(node.getRight().getRight() == null){
+        if (node.getRight().getRight() == null) {
             Node aux = node.getRight();
-            if(node.getRight().getLeft() != null){
+            if (node.getRight().getLeft() != null) {
                 node.setRight(node.getRight().getLeft());
-            }else node.setRight(null);
+            } else {
+                node.setRight(null);
+            }
             return aux;
-        }else{
+        } else {
             return this.takeLastLeftChild(node.getRight());
         }
     }
-    
-    public Node rebalance(Node node){
+
+    public Node rebalance(Node node) {
         this.heightUpdate(node);
         int balancingFactor = this.blancing(node);
-        if (balancingFactor > 1){
-            if(this.height(node.getRight().getRight()) > this.height(node.getRight()
-                    .getLeft())){
+        if (balancingFactor > 1) {
+            if (this.height(node.getRight().getRight()) > this.height(node.getRight()
+                    .getLeft())) {
                 node = this.leftRotate(node);
-            }else{
+            } else {
                 node.setRight(this.rightRotate(node.getRight()));
                 node = this.leftRotate(node);
             }
-        }else if (balancingFactor < -1){
+        } else if (balancingFactor < -1) {
             if (this.height(node.getLeft().getLeft()) > this.height(node.getLeft()
-                    .getRight())){
+                    .getRight())) {
                 node = this.rightRotate(node);
-            }else{
+            } else {
                 node.setLeft(this.leftRotate(node.getLeft()));
                 node = this.rightRotate(node);
             }
         }
         return node;
     }
-    
-    private Node leftRotate(Node node){
+
+    private Node leftRotate(Node node) {
         Node auxR = node.getRight();
         Node auxL = auxR.getLeft();
         auxR.setLeft(node);
@@ -165,8 +175,8 @@ public class BalancedTree {
         this.heightUpdate(auxR);
         return auxR;
     }
-        
-    private Node rightRotate(Node node){
+
+    private Node rightRotate(Node node) {
         Node auxL = node.getLeft();
         Node auxR = auxL.getRight();
         auxL.setRight(node);
@@ -175,36 +185,36 @@ public class BalancedTree {
         this.heightUpdate(auxL);
         return auxL;
     }
-     
-    public int height(Node n){
+
+    public int height(Node n) {
         return n == null ? -1 : n.getHeight();
     }
-    
-    private void heightUpdate(Node n){
+
+    private void heightUpdate(Node n) {
         n.setHeight(1 + Math.max(this.height(n.getLeft()), this.height(n.getRight())));
     }
-    
-    private int blancing(Node n){
+
+    private int blancing(Node n) {
         return n == null ? 0 : this.height(n.getRight()) - this.height(n.getLeft());
     }
-    
-    private boolean isEmpty(Node node){
+
+    private boolean isEmpty(Node node) {
         return node == null;
     }
-    
+
     public Node getRoot() {
         return this.root;
     }
-       
+
     private void setRoot(Node root) {
         this.root = root;
     }
-    
-    public void clear(int v){
+
+    public void clear(int v) {
         this.root.setHeight(0);
         this.root.setLeft(null);
         this.root.setRight(null);
-        this.root.setValue(v);
+        this.root.setHash_id(v);
     }
-    
+
 }
